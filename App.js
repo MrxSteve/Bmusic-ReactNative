23151e17bf4195d39debc2176a7f78f7bb48f1b0
mrxsteve <steventobar38@gmail.com>
2025-07-28 19:34:13 -0600

    feat: config inicial primer proyecto

---
+++ App.js
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={{marginTop: 100, marginLeft: 100}}>
+      <Text style={{fontSize: 20}}>Hello world!</Text>
+    </View>
+  );
+}
