de8be8b5f40aba9827017b043ee473d6367ec25e
mrxsteve <steventobar38@gmail.com>
2025-07-28 20:32:17 -0600

    feat: primera practica

--- App.js
+++ App.js
@@ -1,9 +1,38 @@
-import { Text, View } from "react-native";
+import { Text, View, Image, Button, TouchableOpacity } from "react-native";
 
 export default function App() {
+
+  const userName= 'Steve Tobar';
+
   return (
-    <View style={{marginTop: 100, marginLeft: 100}}>
-      <Text style={{fontSize: 20}}>Hello world!</Text>
+    <View style={{borderWidth: 4, borderColor: 'red', flex: 1}}>
+      <Text style={{fontSize: 20, textAlign: 'center', marginTop: 30}}>
+        BMusicApp
+      </Text>
+        <View style={{borderWidth: 4, borderColor: 'green', padding: 5, marginTop: 20, flexDirection: 'row'}}>
+          <Image 
+            source={{uri: 'https://avatars.githubusercontent.com/u/89896025?v=4'}}
+            style={{height: 100, width: 100, borderRadius: 50}}    
+          />
+          <Text style={{fontSize: 20, textAlign: 'center', 
+            flex: 1, alignSelf: 'center'}}>
+              User: {userName}
+          </Text>
+        </View>
+        <TouchableOpacity
+            style={{borderWidth: 1, backgroundColor: 'blue', marginTop: 10,
+              width: 150, height: 45, alignSelf: 'center', borderRadius: 10, justifyContent: 'center'
+            }}
+            onPress={() => alert("editando...")}
+        >
+            <Text style={{fontSize: 20, textAlign: 'center', color: 'white'}}>Editar</Text>
+        </TouchableOpacity>
+
+        <View>
+          <Text style={{fontSize: 20, textAlign: 'center', marginTop: 30}}>
+            Canciones favoritas
+          </Text>
+        </View>
     </View>
   );
 }
